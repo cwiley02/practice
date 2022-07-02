2a46f691815c460bfb33f937124dde6c54c489fc
hundi <hundi125f@proton.me>
2022-07-02 00:14:08 -0400

    Add files via upload
    I did this by myself, and I am honestly pretty proud of it. Still more to learn though

---
+++ introduction.py
@@ -0,0 +1,4 @@
+name = "Hundi"
+height = "five foot four inches"
+occupation = "suck dick and get knotted"
+print("Hi! My name is"+ " "+name+" "+ "I am"+" "+height+" "+"and I really like to"+" "+ occupation)
