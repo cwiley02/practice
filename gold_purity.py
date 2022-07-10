09b9afee1839045309f579c1026e65290654adc7
Hundi125f <c.wiley.cs@proton.me>
2022-07-10 19:23:05 -0400

    Update gold_purity.py
    improved gold purity checker

--- gold_purity.py
+++ gold_purity.py
@@ -1,11 +1,11 @@
 # Code to test gold purity of 22K (> 91.7%) and 24K (>= 99.9%)
 
 purity = float(input())
-if purity > 91.7:
-    print("Accepted")
-else:
-    if purity < 91.7:
-        print("Not accepted")
-    else:
-        if purity >= 99.9:
-            print("Accepted")
+if purity >= 75.0 and purity < 83.3:
+    print("18K")
+if purity >= 83.3 and purity < 91.7:
+    print("20K")
+if purity >= 91.7 and purity < 99.9:
+    print("22K")
+if purity > 99.9:
+    print("24K")
