63435abf24c27d73496f4cd4b4141d7a1ee06b55
Hundi125f <c.wiley.cs@proton.me>
2022-07-13 06:34:41 -0400

    Changing things
    Honestly, I don't know what all I changed here. I'm new to this

---
+++ Boolean logic.py
@@ -0,0 +1,9 @@
+purity = float(input())
+if purity >= 75.0 and purity < 83.3:
+    print("18K")
+if purity >= 83.3 and purity < 91.7:
+    print("20K")
+if purity >= 91.7 and purity < 99.9:
+    print("22K")
+if purity > 99.9:
+    print("24K")
